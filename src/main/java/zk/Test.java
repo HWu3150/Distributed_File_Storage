2007c8af3df707b4e5a8cac262c86eb05f37d827
Gzq <ziqi.guo@ucdconnect.ie>
2024-12-10 19:58:37 -0500

    html

--- src/main/java/zk/Test.java
+++ src/main/java/zk/Test.java
@@ -1,22 +1,40 @@
 package zk;
 
+import Jpaxos.FileStorageService;
+import lsr.common.Configuration;
 import org.apache.zookeeper.KeeperException;
 
 import java.io.IOException;
 import java.util.List;
 
-public class test {
+
+public class Test {
     public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
         ZKConnectionManager zkConnectionManager = new ZKConnectionManager();
         zkConnectionManager.connect();
-        ServiceRegistry serviceRegistry = new ServiceRegistry(zkConnectionManager.getZooKeeper(), "distributed_file_system");
-        serviceRegistry.registerService("127.0.0.1:7089");
-        serviceRegistry.registerService("127.0.0.1:70001");
-        serviceRegistry.registerService("127.0.0.1:70002");
 
-        ServiceDiscovery serviceDiscovery = new ServiceDiscovery(zkConnectionManager.getZooKeeper(), "distributed_file_system");
+        String serviceName = "distributed_file_system";
+
+        Configuration config = new Configuration("jpaxos.properties");
+        JNode jNode = new JNode(config, 0, new FileStorageService(0), serviceName, "NODE0", zkConnectionManager);
+
+        jNode.start();
+        Monitor monitor0 = new Monitor(zkConnectionManager.getZooKeeper(), jNode.getFullPath());
+        monitor0.startMonitoring();
+
+        ServiceDiscovery serviceDiscovery = new ServiceDiscovery(zkConnectionManager.getZooKeeper(), serviceName);
         List<String> list = serviceDiscovery.discoverServices();
         System.out.println(list);
+
+
+        Thread.sleep(6000);
+        System.out.println("simulate start...");
+        jNode.forceExit();
+
+
+        ServiceDiscovery serviceDiscovery1 = new ServiceDiscovery(zkConnectionManager.getZooKeeper(), serviceName);
+        List<String> list1 = serviceDiscovery1.discoverServices();
+        System.out.println(list1);
 
     }
 }
